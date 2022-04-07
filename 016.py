1941bdb63c480c6fede7df21b7d2fc07c560cfe7
Roberto César <robertocoliver@protonmail.com>
2022-04-06 22:23:29 -0300

    trunk um número ( mostrar a parte inteira )

---
+++ 016.py
@@ -0,0 +1,4 @@
+from math import trunc
+num = float(input('Digite um número: '))
+r = trunc(num)
+print(f'A porção inteira do número {num} é: {r}')
--- exe16.py
+++
@@ -1,8 +0,0 @@
-#modulos
-#import x ( importará tudo da biblioteca )
-#from (biblioteca) import ( item da biblioteca )
-#random.random // random.randint
-from math import trunc
-num = float(input('Digite um número: '))
-r = trunc(num)
-print(f'A porção inteira do número {num} é: {r}')
